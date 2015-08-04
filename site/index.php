94a17d0e1a918905eedb640d53bef64c0b4520da
Jason Hemmy <jasonhemmy@gmail.com>
2015-08-04 15:04:37 -0500

    new site

---
+++ site/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>jasonhemmy</title>
+<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+<link rel="stylesheet" href="fonts.css">
+<link rel="stylesheet" href="main.css">
+</head>
+<body>
+<div>
+<!--///////////////////////////////////////-->
+
+<video autoplay poster="/img/frame.jpg" class="bgvid">
+	<source src="/video/waves.webm" type="video/webm">
+	<source src="/video/waves.mp4" type="video/mp4">
+		<source src="/video/waves.ogg" type="video/ogg">
+</video>
+<div class="overlay"></div>
+
+<div class="band">
+	<a href="https://github.com/hembone" target="_blank"><span class="icon-github"></span></a>
+	<a href="https://www.linkedin.com/in/jasonhemmy" target="_blank"><span class="icon-linkedin"></span></a>
+	<a href="mailto:jasonhemmy@gmail.com" target="_top"><span class="icon-mail"></span></a>
+</div>
+
+<!--///////////////////////////////////////-->
+</div>
+<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="main.js"></script>
+</body>
+</html>
